d5bffefe996d60d5050b30aeeac39b567e82564f
Trevor Adams <trevorwadams@outlook.com>
2019-08-12 16:20:15 -0600

    task: fix typo

--- gatsby-starter-post-theme/gatsby-config.js
+++ gatsby-starter-post-theme/gatsby-config.js
@@ -7,17 +7,17 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `My Blog`,
+    author: `Trevor W. Adams`,
+    description: `My blog description...`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/trevorwadams`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/trevorwadams`,
       },
     ],
   },
